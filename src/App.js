88997ddadd22f1a39710322dc0654ee4c164fb22
zoltanlederer <zoltanlederer@yahoo.com>
2020-09-16 19:48:50 +0200

    Initial update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Reddit extends React.Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://www.reddit.com/r/${this.props.subreddit}.json`
+        )
+      .then(res => {
+        const posts = res.results.map(obj => obj.data);
+        this.setState({ posts });
+      });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div>
+        <h1>{`/r/${this.props.subreddit}`}</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+export default Reddit;
+
+/*
+fetch(`http://www.reddit.com/r/reactjs.json`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed');
+  })
+  .then(json => {
+    const posts = res.data.data.children.map(
+      obj => obj.data
+    );
+    console.log(posts);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+*/
